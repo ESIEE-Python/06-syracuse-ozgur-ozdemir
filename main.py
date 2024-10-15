0a0abfbe3ab639bce05abc990e2e2df9ea606f32
ozgur-ozdemir <121886354+ozgur-ozdemir@users.noreply.github.com>
2024-10-15 06:56:22 +0000

    06-syracuse-ozgur-ozdemir

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+06 syracuse-ozgur-ozdemir
+"""
+
 #### Fonctions secondaires
 
 
@@ -6,6 +10,7 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """Affiche le graphique de la suite de syracuse"""
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -32,8 +37,14 @@ def syracuse_l(n):
         list: la suite de Syracuse de source n
     """
 
-    # votre code ici 
-    l = [ ]
+    # votre code ici
+    l = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +56,8 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
     # votre code ici
-
-    n = 0
-    return n
+    return len(l)
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -60,11 +68,12 @@ def temps_de_vol_en_altitude(l):
     Returns:
         int: le temps de vol en altitude
     """
-
-    # votre code ici
-
-    n = 0
-    return n
+    altitude_initiale = l[0]
+    # Parcourir la liste et arrêter dès qu'une valeur inférieure à l'altitude initiale est trouvée
+    for i, valeur in enumerate(l):
+        if valeur < altitude_initiale:
+            return i
+    return len(l)
 
 
 def altitude_maximale(l):
@@ -76,18 +85,16 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
+
     # votre code ici
-    
-    n = 0
-    return n
+    return max(l)
 
 
 #### Fonction principale
 
 
 def main():
-
+    """ fonction principale"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
